7c50bb1ad6347a201f214f375ca0ec2d3b8674e8
Eufrasio Daniel <edaniel@indracompany.com>
2022-04-28 11:41:51 -0300

    inicio twitter clone

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -11,6 +11,11 @@ class IndexController extends Action {
 	public function index() {
 		$this->render('index');
 	}
+
+	public function inscreverse() {
+		$this->render('inscreverse');
+	}
+
 }
 
 ?>
--- App/Route.php
+++ App/Route.php
@@ -11,6 +11,13 @@
 				'controller' => 'indexController',
 				'action' => 'index'
 			);
+
+			$routes['inscreverse'] = array(
+				'route' => '/inscreverse',
+				'controller' => 'indexController',
+				'action' => 'inscreverse'
+			);
+			
 			
 			$this->setRoutes($routes);
 		}
